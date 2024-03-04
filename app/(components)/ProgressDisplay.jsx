87acaee6c46db092fe3fa1a829be1e1c3dae1a68
aashmaaa <109813735+aashmaaa@users.noreply.github.com>
2024-03-04 22:31:12 +0545

    initial commit

---
+++ app/(components)/ProgressDisplay.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProgressDisplay = () => {
+  return (
+    <div className="w-full bg-gray-200 rounded-full h-2.5">ProgressDisplay</div>
+  );
+};
+
+export default ProgressDisplay;
