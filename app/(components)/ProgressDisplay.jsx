2b9abd2f352b4f7c0eb7714d9d8d1292a533c88f
aashmaaa <109813735+aashmaaa@users.noreply.github.com>
2024-03-20 16:58:05 +0545

    CSS update

--- app/(components)/ProgressDisplay.jsx
+++ app/(components)/ProgressDisplay.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const ProgressDisplay = () => {
   return (
-    <div className="w-full bg-gray-200 rounded-full h-2.5">ProgressDisplay</div>
+    <div className="w-full bg-gray-200 rounded-full h-2.5">
+      <div
+        className="bg-blue-600 h-2.5 rounded-full"
+        style={{ width: "75%" }}
+      ></div>
+    </div>
   );
 };
 
---
+++ app/(components)/StatusDisplay.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function StatusDisplay() {
+  return (
+    <div className="inline-block rounded-full px-2 py-1 text-xs font-semihold text-gray-700 bg-green-200">
+      Done
+    </div>
+  );
+}
+
+export default StatusDisplay;
