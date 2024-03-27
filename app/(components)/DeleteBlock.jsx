6acfbccf0b0cb611290702bb2f6b83db5fea228b
aashmaaa <109813735+aashmaaa@users.noreply.github.com>
2024-03-27 22:42:51 +0545

    updated delete function

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+"use client";
+
 import { faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/router";
 
-function DeleteBlock() {
+const DeleteBlock = ({ id }) => {
+  const router = useRouter();
+
+  const deleteTicket = async () => {
+    const res = await fetch(`http:/localhost:3000/api/Tickets/${id}`, {
+      method: "DELETE",
+    });
+
+    if (res.ok) {
+      router.push("/");
+      router.refresh();
+    }
+  };
+
   return (
     <FontAwesomeIcon
       icon={faX}
       className="text-red-400 hover:cursor-pointer hover:text-red-200"
+      onClick={deleteTicket}
     />
   );
-}
+};
 
 export default DeleteBlock;
